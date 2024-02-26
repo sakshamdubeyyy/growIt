a5030076709251ec6f201e75dc49cbc7de698191
sakshamdubeyyy <109180511+sakshamdubeyyy@users.noreply.github.com>
2024-02-26 06:13:33 +0530

    ok

---
+++ src/Routes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+const About = React.lazy(() => import("pages/About"));
+const Pricing = React.lazy(() => import("pages/Pricing"));
+const Homepage = React.lazy(() => import("pages/Homepage"));
+const SignUp = React.lazy(() => import("pages/SignUp"));
+const Login = React.lazy(() => import("pages/Login"));
+const ProjectRoutes = () => {
+  return (
+    <React.Suspense fallback={<>Loading...</>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dhiwise-dashboard" element={<Home />} />
+        </Routes>
+      </Router>
+    </React.Suspense>
+  );
+};
+export default ProjectRoutes;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+import { Button, Img, Text } from "components";
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={props.className}>
+        <div className="flex md:flex-col flex-row md:gap-5 items-center justify-center p-[15px] w-full">
+          <div className="header-row my-2.5">
+            <div className="flex flex-row gap-4 items-end justify-center">
+              <Link to="/Homepage">
+              <Img
+                className="h-[100px] w-[100px]"
+                src="images/img_uilchartgrowt.svg"
+                alt="Group448"
+              />
+              </Link>
+            </div>
+            <div className="mobile-menu">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className="flex md:flex-1 sm:flex-col flex-row gap-10 sm:hidden items-start justify-center md:ml-[0] ml-[78px] w-[30%] md:w-full">
+            <Link to="/Pricing">
+            <Text
+              className="text-base text-gray-900"
+              size="txtChivoBold16Gray900"
+            >
+              Pricing
+            </Text>
+            </Link>
+            <Link to="/About">
+            <Text
+              className="text-base text-gray-900"
+              size="txtChivoBold16Gray900"
+            >
+              About
+            </Text>
+            </Link>
+            <Link to="/About">
+            <Text
+              className="text-base text-gray-900"
+              size="txtChivoBold16Gray900"
+            >
+              Contact
+            </Text>
+            </Link>
+          </div>
+          <div className="flex md:flex-1 flex-row gap-10 sm:hidden items-center justify-center md:ml-[0] ml-[290px] rounded-md w-[15%] md:w-full">
+          <Link to="/">
+            <Text
+              className="text-base text-gray-900"
+              size="txtChivoBold16Gray900"
+            >
+              Login
+            </Text>
+          </Link>
+          <Link to="/SignUp">
+            <Button
+              className="cursor-pointer font-bold font-chivo min-w-[122px] text-base text-center"
+              shape="round"
+              color="teal_400"
+              size="xs"
+              variant="fill"
+            >
+              Sign Up
+            </Button>
+          </Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+Header.defaultProps = {};
+
+export default Header;
